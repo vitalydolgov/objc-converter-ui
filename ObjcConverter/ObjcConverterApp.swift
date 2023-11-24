3d04cedd68e72b1644da91ecea0ed2c92f65091c
Vitaly Dolgov <vitaly.dolgov@posteo.net>
2023-11-24 21:46:23 +0100

    Initial Commit

---
+++ ObjcConverter/ObjcConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjcConverterApp.swift
+//  ObjcConverter
+//
+//  Created by Vitaly Dolgov on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ObjcConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
